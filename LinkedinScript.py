ef223b8f43f764363cdb4f68c900da873ccea1f9
Amit Roth <amitroth@gmail.com>
2020-07-25 13:41:05 +0300

    2nd commit

--- LinkedinScript.py
+++ LinkedinScript.py
@@ -1,1 +1,12 @@
-#LINKED IN AUTO CONNECTIONS
+import os
+from selenium import webdriver
+
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DRIVER_PATH = os.path.join(BASE_DIR, 'chromedriver')
+
+
+driver = webdriver.Chrome(DRIVER_PATH)
+
+driver.get("https://www.linkedin.com/feed/")
